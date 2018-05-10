c2159f3c3e292925155206b2b90613b09f38d27c
Harrison <joshua.j.harrison@intel.com>
2018-05-10 16:10:54 -0700

    chore: set up reusable data

---
+++ src/app/user.model.ts
@@ -0,0 +1,7 @@
+export class User {
+  Idsid: string;
+  FirstNm: string;
+  LastNm: string;
+  FullNm: string;
+  WWID: string;
+}
---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const config = {
+  workerUrl: 'https://workers.intel.com/Services/hrdataservices/api/Worker/GetWorker',
+};
