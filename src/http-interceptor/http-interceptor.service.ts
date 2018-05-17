97121aa7371b4cd7090af6ad1ed6b10c934c467d
Harrison <joshua.j.harrison@intel.com>
2018-05-17 09:56:51 -0700

    refactor: return worker mock always

--- src/http-interceptor/http-interceptor.service.ts
+++ src/http-interceptor/http-interceptor.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpHandler, HttpEvent, HttpRequest } from '@angular/common/http';
+import { HttpInterceptor, HttpHandler, HttpEvent, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
 import { Worker } from '../mocks.data';
 
 @Injectable()
@@ -10,9 +12,17 @@ export class HttpInterceptorService  implements HttpInterceptor {
   constructor() { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(request).do(
-      event => event,
-      error => Worker.clean,
-    );
+    return next.handle(request)
+      .do(
+        event => {
+          return Worker.clean;
+        }
+      )
+      .catch(
+        error => {
+          const httpResponse = new HttpResponse({body: [Worker.clean]});
+          return Observable.of(httpResponse);
+        }
+      );
   }
 }
